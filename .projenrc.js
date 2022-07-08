4dd6e3c28d19f7662cae221e3564c1dbb5d3bb93
mandalvesq <mandaquinto@gmail.com>
2022-07-07 23:43:24 -0300

    (chore): adding projen files

---
+++ .projenrc.js
@@ -0,0 +1,99 @@
+const { AwsCdkConstructLibrary, DevEnvironmentDockerImage, Gitpod } = require('projen');
+
+const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
+
+const project = new AwsCdkConstructLibrary({
+  author: 'Pahud Hsieh',
+  authorAddress: 'pahudnet@gmail.com',
+  description: 'CDK construct library that allows you to create KeyCloak service on AWS in TypeScript or Python',
+  cdkVersion: '2.0.0',
+  jsiiFqn: 'projen.AwsCdkConstructLibrary',
+  name: 'cdk-keycloak',
+  repositoryUrl: 'https://github.com/aws-samples/cdk-keycloak.git',
+  cdkDependencies: [
+    'aws-cdk-lib/aws-autoscaling',
+    'aws-cdk-lib/aws-certificatemanager',
+    'aws-cdk-lib/aws-ec2',
+    'aws-cdk-lib/aws-ecs',
+    'aws-cdk-lib/aws-elasticloadbalancingv2',
+    'aws-cdk-lib/aws-iam',
+    'aws-cdk-lib/aws-logs',
+    'aws-cdk-lib/aws-rds',
+    'aws-cdk-lib/aws-secretsmanager',
+    'aws-cdk-lib',
+  ],
+  devDeps: [
+    '@types/prettier@<2.6.0',
+  ],
+  // jestOptions: { jestVersion: '^28.0.0' },
+  depsUpgradeOptions: {
+    ignoreProjen: false,
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      secret: AUTOMATION_TOKEN,
+    },
+  },
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['pahud'],
+  },
+  catalog: {
+    announce: false,
+    twitter: 'pahudnet',
+  },
+  defaultReleaseBranch: 'main',
+  publishToPypi: {
+    distName: 'cdk-keycloak',
+    module: 'cdk_keycloak',
+  },
+  keywords: [
+    'cdk',
+    'keycloak',
+    'aws',
+  ],
+});
+
+project.package.addField('resolutions', {
+  'pac-resolver': '^5.0.0',
+  'set-value': '^4.0.1',
+  'ansi-regex': '^5.0.1',
+});
+
+
+const gitpodPrebuild = project.addTask('gitpod:prebuild', {
+  description: 'Prebuild setup for Gitpod',
+});
+// install and compile only, do not test or package.
+gitpodPrebuild.exec('yarn install --frozen-lockfile --check-files');
+gitpodPrebuild.exec('npx projen compile');
+
+let gitpod = new Gitpod(project, {
+  dockerImage: DevEnvironmentDockerImage.fromImage('public.ecr.aws/pahudnet/gitpod-workspace:latest'),
+  prebuilds: {
+    addCheck: true,
+    addBadge: true,
+    addLabel: true,
+    branches: true,
+    pullRequests: true,
+    pullRequestsFromForks: true,
+  },
+});
+
+gitpod.addCustomTask({
+  init: 'yarn gitpod:prebuild',
+  // always upgrade after init
+  command: 'npx projen upgrade',
+});
+
+gitpod.addVscodeExtensions(
+  'dbaeumer.vscode-eslint',
+  'ms-azuretools.vscode-docker',
+  'AmazonWebServices.aws-toolkit-vscode',
+);
+
+const common_exclude = ['cdk.out', 'cdk.context.json', 'images', 'yarn-error.log', 'dependabot.yml'];
+project.npmignore.exclude(...common_exclude);
+project.gitignore.exclude(...common_exclude);
+
+
+project.synth();
